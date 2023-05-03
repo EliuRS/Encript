b9e1b9283c5e939963ce4071192a96bfcfde050b
Eliu Santos <rosalessantos123@gmail.com>
2023-05-02 20:22:46 -0600

    Encript

---
+++ prueba.js
@@ -0,0 +1,12 @@
+var encriptar = document.querySelector(".btn-encriptar");
+var desencriptar = document.querySelector(".btn-desencriptar");
+var copiar = document.querySelector(".btn-copiar");
+var area = document.querySelector(".area");
+var limpiar = document.querySelector(".btn-limpiar");
+
+
+encriptar.onclick = encriptarTexto;
+desencriptar.onclick = desencriptarTexto;
+copiar.onclick = copiarTexto;
+area.onclick = volverAside;
+limpiar.onclick = limpiarArea;
